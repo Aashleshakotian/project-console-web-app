f7173f976e36ee02427097da0ca77156e7367689
Aashleshakotian <77013016+Aashleshakotian@users.noreply.github.com>
2021-03-03 12:37:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var first_project = "My-first-console-project";
+console.log(first_project);
+
+var first_name = "Aashlesha";
+console.log(first_name);
+
+var last_name = "Kotian";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
